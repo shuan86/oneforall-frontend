85ccf7b94ae1d8cb7dff78fb2f83d4c467a418cb
羅士欽 <s490607@gmail.com>
2021-01-17 18:10:12 +0800

    NewsCard切版 差img功能還沒好

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { NewsCardUnreviewed, NewsCardUnderReview, NewsCardReviewed } from './NewsCard';
 
 class App extends Component {
     state = { loading: false };
@@ -14,15 +15,21 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
-                <p>{ this.state.loading.toString() }</p>
-                <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+            // <div className="">
+            //     <div className="App-header">
+            //         {/* <img src={logo} className="App-logo" alt="logo" />
+            //         <h2>Welcome to React</h2> */}
+            //     </div>
+            //     <NewsCard />
+            //     {/* <p>{ this.state.loading.toString() }</p>
+            //     <p className="App-intro">
+            //         To get started, edit <code>src/App.js</code> and save to reload.
+            //     </p> */}
+            // </div>
+            <div>
+                <NewsCardUnreviewed />
+                <NewsCardUnderReview />
+                <NewsCardReviewed />
             </div>
         );
     }
---
+++ src/NewsCard.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import './css/common.css';
+import './css/NewsCard.css';
+// import author from './author.jpg';
+import author from './logo.svg';
+
+const NewsCardContent = ({ status }) => {
+    console.log('NewsCardContent');
+    return (
+        <div className="content">
+            <div className="user-info">
+                <img src={author} alt="Background" className="user-photo" />
+                <div className="article-data">
+                    <div className="user-id">abc12345678</div>
+                    <div className="article-time">2020-11-06 21:24:08</div>
+                </div>
+            </div>
+            <div className="hashtag">
+                <a href=""># 醫療</a>
+                <a href=""># 疫苗</a>
+            </div>
+            <div className="article">
+                <h3>這應該大概有十個字吧還是應該要再多一點字呢呢呢呢呢呢呢</h3>
+                <p>中央流行疫情指揮中心日前宣布秋冬專案，要求本國人比照外國人，出具三日內檢驗陰性證明才能登機返國，引起反彈。指揮中心今公布三種例外情況，但若不符合例外情況</p>
+                <a href="">繼續閱讀</a>
+            </div>
+            <div className="like">
+                <div>
+                    <a href="">想知道</a>
+                    <span>123人想知道</span>
+                </div>
+                <div className={status ? 'none' : 'vote'}>
+                    <a href="">同意</a>
+                    <a href="">反對</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const NewsCardComment = () => {
+    console.log('NewsCardComment');
+    return (
+        <div className="comment">
+            <div className="user-input">
+                <img src={author} alt="Background" className="user-photo" />
+                <input type="text" placeholder="告訴我們你的想法" />
+                <a href="">傳送</a>
+            </div>
+            <div className="user-comment">
+                <img src={author} alt="Background" className="user-photo" />
+                <span>
+                    <div className="user-id">abc12345678</div>
+                    <p>大問號？！！！</p>
+                </span>
+                <p>20min</p>
+            </div>
+            <a href="">查看其他留言</a>
+        </div>
+    );
+};
+
+const NewsCard = ({ status }) => {
+    console.log('NewsCard');
+    console.log('NewsCardUnreviewed', status);
+    return (
+        <div className="card">
+            <div className={status ? 'status' : 'none'}>未審核</div>
+            <NewsCardContent status={status} />
+            <NewsCardComment />
+        </div>
+    );
+};
+
+const NewsCardUnreviewed = () => {
+    console.log('NewsCard');
+    console.log('NewsCardUnreviewed', status);
+    return (
+        <NewsCard status />
+    );
+};
+const NewsCardUnderReview = () => {
+    console.log('NewsCard');
+    return (
+        <div className="card">
+            <div className="status">審核中</div>
+            {/* <h1>NewsCard</h1> */}
+            <NewsCardContent />
+            <NewsCard status={false} />
+            <NewsCardComment />
+        </div>
+    );
+};
+const NewsCardReviewed = () => {
+    console.log('NewsCard');
+    return (
+        <div className="card">
+            <div className="status">審核後</div>
+            {/* <h1>NewsCard</h1> */}
+            <NewsCardContent />
+            <NewsCard status={false} />
+            <NewsCardComment />
+        </div>
+    );
+};
+
+export { NewsCardUnreviewed, NewsCardUnderReview, NewsCardReviewed };
