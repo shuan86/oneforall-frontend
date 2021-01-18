e76b05132c32251d9ffdde7fee4fbb9c75e16852
SylviaWang <yaya75315@gmail.com>
2021-01-18 12:26:34 +0800

    nochange

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ class App extends Component {
                 </div>
                 <p>{ this.state.loading.toString() }</p>
                 <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
             </div>
         );
     }
